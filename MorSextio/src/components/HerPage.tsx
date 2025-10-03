1fe2ca7db9e14d1e0fae9a417c016e909bc36acb
Sara <sara_lunden1@hotmail.com>
2025-10-03 11:05:51 +0200

    new styling added

--- MorSextio/src/components/HerPage.tsx
+++ MorSextio/src/components/HerPage.tsx
@@ -1,36 +1,50 @@
 import { Stars } from "./Starsky";
+import one from "./../assets/1.png";
+import two from "./../assets/2.png";
+import three from "./../assets/3.png";
+import four from "./../assets/4.png";
+import five from "./../assets/5.png";
 
 export default function HerPage() {
   return (
     <main className="herPage">
       <Stars />
 
-      <ul className="PictureBox">
-        <li className="pic">
-            <img src="#" alt="#" />
-        </li>
-        <li className="pic">
-            <img src="#" alt="#" />
-        </li>
-        <li className="pic">
-            <img src="#" alt="#" />
-        </li>
-        <li className="pic">
-            <img src="#" alt="#" />
-        </li>
-        <li className="pic">
-            <img src="#" alt="#" />
-        </li>
-        <li className="pic">
-            <img src="#" alt="#" />
-        </li>
-        <li className="pic">
-            <img src="#" alt="#" />
-        </li>
-        <li className="pic">
-            <img src="#" alt="#" />
-        </li>
-      </ul>
+      <div className="scroller">
+        <ul className="PictureBox">
+          <li className="picBoxList">
+            <img src={five} alt="Borås Logga" className="picInBox" />
+          </li>
+          <li className="picBox">
+            <img src={one} alt="Hotell" className="picInBox" />
+          </li>
+          <li className="picBox">
+            <img src={two} alt="Nappy Time" className="picInBox" />
+          </li>
+          <li className="picBox">
+            <img src={three} alt="FOOD" className="picInBox" />
+          </li>
+          <li className="picBox">
+            <img src={four} alt="Hemsökta museet" className="picInBox" />
+          </li>
+
+          <li className="picBox">
+            <img src={five} alt="Borås Logga" className="picInBox" />
+          </li>
+          <li className="picBox">
+            <img src={one} alt="Hotell" className="picInBox" />
+          </li>
+          <li className="picBox">
+            <img src={two} alt="Nappy Time" className="picInBox" />
+          </li>
+          <li className="picBox">
+            <img src={three} alt="FOOD" className="picInBox" />
+          </li>
+          <li className="picBox">
+            <img src={four} alt="Hemsökta museet" className="picInBox" />
+          </li>
+        </ul>
+      </div>
     </main>
   );
 }
