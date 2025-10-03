a6a9eae1fedfdce0399e64a65db6c00bd80722cc
Sara <sara_lunden1@hotmail.com>
2025-10-03 15:28:46 +0200

    new style

--- MorSextio/src/components/HerPage.tsx
+++ MorSextio/src/components/HerPage.tsx
@@ -1,10 +1,11 @@
 import { Stars } from "./Starsky";
-import "./../App.css"
+import "./../App.css";
 import one from "./../assets/1.png";
 import two from "./../assets/2.png";
 import three from "./../assets/3.png";
 import four from "./../assets/4.png";
 import five from "./../assets/5.png";
+import Kuvert from "./Paket";
 
 export default function HerPage() {
   return (
@@ -12,25 +13,29 @@ export default function HerPage() {
       <Stars />
 
       <div className="scroller">
-        <img src={five} alt="Borås Logga" className="picInBox" />
-        <img src={one} alt="Hotell" className="picInBox" />
-        <img src={two} alt="Nappy Time" className="picInBox" />
-        <img src={three} alt="FOOD" className="picInBox" />
-        <img src={four} alt="Hemsökta museet" className="picInBox" />
-        <img src={five} alt="Borås Logga" className="picInBox" />
-        <img src={one} alt="Hotell" className="picInBox" />
-        <img src={two} alt="Nappy Time" className="picInBox" />
-        <img src={three} alt="FOOD" className="picInBox" />
-        <img src={four} alt="Hemsökta museet" className="picInBox" />
-        <img src={five} alt="Borås Logga" className="picInBox" />
-        <img src={one} alt="Hotell" className="picInBox" />
-        <img src={two} alt="Nappy Time" className="picInBox" />
-        <img src={three} alt="FOOD" className="picInBox" />
-        <img src={four} alt="Hemsökta museet" className="picInBox" />
+        <div className="scrollerBox">
+          <img src={five} alt="Borås Logga" className="picInBox" />
+          <img src={one} alt="Hotell" className="picInBox" />
+          <img src={two} alt="Nappy Time" className="picInBox" />
+          <img src={three} alt="FOOD" className="picInBox" />
+          <img src={four} alt="Hemsökta museet" className="picInBox" />
+          <img src={five} alt="Borås Logga" className="picInBox" />
+          <img src={one} alt="Hotell" className="picInBox" />
+          <img src={two} alt="Nappy Time" className="picInBox" />
+          <img src={three} alt="FOOD" className="picInBox" />
+          <img src={four} alt="Hemsökta museet" className="picInBox" />
+          <img src={five} alt="Borås Logga" className="picInBox" />
+          <img src={one} alt="Hotell" className="picInBox" />
+          <img src={two} alt="Nappy Time" className="picInBox" />
+          <img src={three} alt="FOOD" className="picInBox" />
+          <img src={four} alt="Hemsökta museet" className="picInBox" />
+        </div>
       </div>
 
-      <section>
+      <section className="contentBox">
         <h1 className="herPageh1">Grattis vår älskade underbara Helena</h1>
+
+        <Kuvert />
       </section>
     </main>
   );
---
+++ MorSextio/src/components/Paket.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+export default function Kuvert() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <section onClick={() => setOpen(!open)} className="kuvert-box">
+        <div className={`kuvert ${open ? "open" : ""}`}>
+          <div className="kuvert-flärp">
+            <div className="kuvert-ficka">
+              <div className="brevet">
+                <h1 className="bh1">Grattis Helena!</h1>
+                <p>
+                  Det är svårt att komma på något att ge dig när man helst vill
+                  ge dig allt det fina och goda i livet! Men vi hoppas att du
+                  ska tycka detta kan vara ett litet roligt äventyr. Det är
+                  ingen utlandsresa, tyvärr. MEN Tillsammans med din dotter,
+                  jag, ska du få åka på en weekend trip till Borås. Ja undra vad
+                  vi ska göra i Borås? På senare dagar har du ju gått och blivit
+                  litet fan av två män och spöken. Därför har vi sett till att
+                  köpa ett presentkort som kan användas som inträde för museet,
+                  vi kan också fixa så att vi får en guidad tur med bröderna
+                  LAXTON själva. Ska vi vara lte extra modiga så kan vi även
+                  köra vår egna lilla spökutredning, eller vill vi verkligen
+                  köra på ett wild card, varför inte åka till självaste
+                  Prästgården / Borgvattnet. Men tillsammans kan vi lista ut
+                  exakt hur och när och vad såklart. MEN det är inte slut där,
+                  utöver presentkortet har jag tänkt att vi ska ta in på hotell,
+                  lyxigt och mysigt, kanske gå ut och käka och bara ha det bra.
+                  Jag, din dotter, kör till och från Borås, mellan hotell och
+                  Laxton Museet, Förhoppningsvis i din bil, hehe! Det finns en
+                  Helena 60 år "kassa" som många av oss varit med och bidragit
+                  till och denna kan vi alltså röra oss med för din trivsel. Jag
+                  kommer att stå för mig själv.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
